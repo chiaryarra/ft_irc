5f4f189b7d6bf166d2c908c864e597990c77aa1e
hebatist <hebaja@yahoo.com.br>
2026-04-14 20:44:09 -0300

    feat(NICK): Setting client nickname
    - NICK.cpp populated
    - Condition to get NICK command included in Server.cpp

--- includes/utils/Utils.hpp
+++ includes/utils/Utils.hpp
@@ -8,7 +8,7 @@
 
 bool	authPass(Client &client, std::string password, std::string server_password);
 void	setClientNick(std::string nickname, Client &client);
-bool	auth_pass(std::vector<std::string> split_msg, Client &client, std::string password);
 bool	authPass(Client &client, std::string password, std::string server_password);
+void	setClientNick(std::string nickname, Client &client);
 
 #endif
