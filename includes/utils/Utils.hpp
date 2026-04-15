68ccb490ae47f921186aa6638e6d0f440d1df166
hebatist <hebaja@yahoo.com.br>
2026-04-14 21:00:56 -0300

    fix(PASS): general branch optimization
    - authPass function changed
    - PASS.hpp removed
    - authPass prototype moved to Utils.hpp
    Closes #

--- includes/utils/Utils.hpp
+++ includes/utils/Utils.hpp
@@ -3,10 +3,12 @@
 
 # include <iostream>
 # include <vector>
+# include <string>
 # include "../client/Client.hpp"
 
 bool	authPass(Client &client, std::string password, std::string server_password);
 void	setClientNick(std::string nickname, Client &client);
 bool	auth_pass(std::vector<std::string> split_msg, Client &client, std::string password);
+bool	authPass(Client &client, std::string password, std::string server_password);
 
 #endif
