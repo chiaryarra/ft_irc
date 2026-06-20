abdb2adabbd72bcb5c7e9d02ca7fc7e904de363f
Henrique <hebaja@yahoo.com.br>
2026-06-20 16:40:14 -0300

    feat(messages): include numeric constants

--- includes/utils/Utils.hpp
+++ includes/utils/Utils.hpp
@@ -6,6 +6,23 @@
 # include <string>
 # include "../client/Client.hpp"
 
+const std::string	RPL_WELCOME = "001";
+const std::string	RPL_YOURHOST = "002";
+const std::string	RPL_CREATED = "003";
+const std::string	RPL_MYINFO = "004";
+
+const std::string	ERR_UNKNOWNCOMMAND = "421";
+const std::string	ERR_NONICKNAMEGIVEN = "431";
+const std::string	ERR_ERRONEUSNICKNAME = "432";
+const std::string	ERR_NICKNAMEINUSE = "433";
+const std::string	ERR_NOTREGISTERED = "451";
+const std::string	ERR_NEEDMOREPARAMS = "461";
+const std::string	ERR_ALREADYREGISTRED = "462";
+const std::string	ERR_PASSWDMISMATCH = "464";
+
+const std::string	MSG_NEEDMOREPARAMS = ":Not enough parameters";
+
+
 bool	authPass(Client &client, std::string password, std::string server_password);
 void	setClientNick(std::string nickname, Client &client);
 bool	setClientUsername(std::string message, std::vector<std::string> split_msg, Client &client);
