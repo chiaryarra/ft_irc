f563ea950006b0e8571f4a36396317b552cf4ac0
Henrique <hebaja@yahoo.com.br>
2026-07-19 17:25:14 -0300

    feat(kick): add kick function to header

--- includes/utils/Utils.hpp
+++ includes/utils/Utils.hpp
@@ -64,6 +64,7 @@ std::string joinChannel(Client &client, std::string channelName, bool isNewChann
 std::string checkChannelMode(Channel &channel, int clientFd, bool isKeyPass);
 std::string manageChannelMode(Channel &channel, std::string modes, std::vector<std::string> &params, std::map<int, Client> &clients, bool isMember, bool isOperator, std::string &modeChange);
 std::string inviteUser(Client &client, Client &target, Channel &channel);
+std::string	manangeKickCommand(Client &client, Channel &channel, Client &target);
 std::map<int, Client>::iterator findClientByNick(std::map<int, Client> &clients, const std::string &nick);
 
 #endif
